2fb123d2e6a9924407fac60ed35f94ef3751af19
AxelESSONO <naxelcdric@gmail.com>
2025-05-12 15:29:53 +0200

    Initial commit

---
+++ app/src/main/java/com/eseo/productapp/view/ProductActivity.kt
@@ -0,0 +1,19 @@
+package com.eseo.productapp.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.eseo.productapp.ui.theme.ProductAppTheme
+
+class ProductActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProductAppTheme {
+
+            }
+        }
+    }
+}
